a10e5acb0cfa6180f777d2e9c477e163bb14ec4b
Ryan Token <ryantoken13@gmail.com>
2025-09-17 21:24:46 -0600

    Add head metadata to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
-
 <script>
 	import BookingForm from '$lib/BookingForm.svelte';
 	import Section from '$lib/components/Section.svelte';
@@ -6,8 +5,13 @@
 	import Spacer from '$lib/components/Spacer.svelte';
 </script>
 
+<svelte:head>
+  <title>Semper Finish</title>
+  <meta name="description" content="Semper Finish delivers professional painting and handyman jobs to the St. Louis metro area." />
+</svelte:head>
+
 <Section>
-	<Heading title="Semper Finish" lead="We’re a local crew delivering quality work across the St. Louis metro area. Painting & precision you can trust. Your home. Our honor." />
+	<Heading title="Semper Finish" lead="We’re a local crew delivering quality work across the St. Louis metro area. Woman-owned and veteran-operated, we bring painting and precision you can trust. Your home. Our honor." />
 	<Spacer />
 	<BookingForm />
 </Section>
